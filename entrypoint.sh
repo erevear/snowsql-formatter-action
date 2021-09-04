c17f946d424fae0f23ca2b5b5cabde5efda21012
Eva Revear <eva.revear@pieinsurance.com>
2021-09-03 20:30:31 -0700

    change snowsql install

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ echo "whyyyy"
 
 # bash -c "sql-formatter -h"
 # bash -c "hello test.sql"
-for file in `find . -name '*.sql'`; do
+# for file in `find . -name '*.sql'`; do
+for file in find ./ -iname '*.sql' -type f
     echo 'running'
     hello $file
     # hello $file -o $file
