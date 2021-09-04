eef9cb6e1e9129e30b7a28ee04dcd7cfe3ec07f3
Eva Revear <eva.revear@pieinsurance.com>
2021-09-03 20:39:32 -0700

    looping files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "whyyyy"
 # bash -c "sql-formatter -h"
 # bash -c "hello test.sql"
 # for file in `find . -name '*.sql'`; do
-for file in 'find ./ -iname '*.sql' -type f'; do
+for file in `find ./ -iname '*.sql' -type f`; do
     echo "running"
     echo $file
     # hello $file -o $file
