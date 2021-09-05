94cf8ad66ce5411d24f64d5ccc120931857495b4
Eva Revear <eva.revear@pieinsurance.com>
2021-09-04 18:47:09 -0700

    removing print statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 set -e
-echo --recursive $GITHUB_WORKSPACE $*
-echo ${INPUT_DIRECTORY:-'.'}
+# echo --recursive $GITHUB_WORKSPACE $*
+# echo ${INPUT_DIRECTORY:-'.'}
 # FILES=$(curl -s -X GET -G $URL | jq -r '.[] | .filename')
 # bash -c "sql-formatter -h"
 # bash -c "hello test.sql"
 for file in `find . -name '*.sql'`; do
-    echo "running first"
-    echo $file
-    snowsql-formatter $file -o $file
+    echo "Formatting " +  $file
+    snowsql-formatter "'" + $file + "'" -o  "'" + $file + "'"
 done 
 
-for file in `find ./ -iname '*.sql' -type f`; do
-    echo "running"
-    echo "'" + $file + "'" -o  "'" + $file + "'"
-    # hello $file -o $file
-done
+# for file in `find ./ -iname '*.sql' -type f`; do
+#     echo "running"
+#     echo "'" + $file + "'" -o  "'" + $file + "'"
+#     # hello $file -o $file
+# done
 
-cd ${INPUT_DIRECTORY}
+# cd ${INPUT_DIRECTORY}
 
 # remote_repo="https://${GITHUB_ACTOR}:${INPUT_GITHUB_TOKEN}@github.com/${REPOSITORY}.git"
 
